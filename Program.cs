f8a9bac2a51d6ef2e802a7caad73be3b26ca373a
dovidHodes <dovidhodes@g,ail.com>
2023-06-21 18:07:08 -0400

    added code to handel unrecognized column names, started making methods that create the new spreadsheets

--- Program.cs
+++ Program.cs
@@ -1,43 +1,88 @@
 ﻿using ClosedXML.Excel;
-using DocumentFormat.OpenXml.Drawing;
-using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace shopifyNonSeasonalFormatter
 {
-
-    //actually. can use the color metafeild for when end user does select product has variants, just cjeck if the size coulumn exists, if not then, we can use color from metafield
     internal class Program
     {
         static string sourceFilePath = @"C:\Users\User\Desktop\test.xlsx";
         static IXLWorksheet? sourceWorksheet;
         static int lastRow;
         static int lastColumn;
-        //static int columnCount = 0;
+        static column[] columns = new column[11];
+        static bool needStoneEdgeSpreadsheet = true;
+        static bool needShopifySpreadsheet = true;
         static void Main(string[] args)
         {
-            
-            column[] columns = new column[11];
+            importDataFromSourceFile();
 
+            if(needStoneEdgeSpreadsheet)
+            {
+                createStoneEdgeSpreadsheet();
+            }
+            if(needShopifySpreadsheet)
+            {
+                createShopifySpreadsheet();
+            }
+
+            foreach (column column in columns)
+            {
+                if (column != null)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine(column.columnName.ToUpper());
+                    Console.WriteLine();
+                    foreach (IXLCell cell in column.rows.Cells())
+                    {
+                        Console.WriteLine(cell.Value);
+                    }
+                }
+            }
+        }
+
+
+        static column createNewColumnObject(string columnName, int columnNUmber)
+        {
+            //
+            // Makes the range with the (row, column, row, column) overload
+            //
+            IXLRange rows = sourceWorksheet.Range(2, columnNUmber, lastRow, columnNUmber);
+            column newColumn = new column(columnName, rows);
+
+            return newColumn;
+        }
+
+        static void showAlert(string bigMessage, string smallMessage)
+        {
+            Console.WriteLine();
+            Console.WriteLine(smallMessage);
+            Console.WriteLine(bigMessage);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static void importDataFromSourceFile()
+        //
+        // gets the columns from the sheet and if they're not empty gets the contents of each column and
+        // puts the range into the right slot in the column array, by putting it into the slot of that enum number
+        //
+        {
             using (var workbook = new XLWorkbook(sourceFilePath))
             {
                 sourceWorksheet = workbook.Worksheet(1);
                 lastColumn = sourceWorksheet.LastColumnUsed().ColumnNumber();
                 lastRow = sourceWorksheet.LastRowUsed().RowNumber();
 
-                //
-                // gets the columns from the sheet and if they're not empty gets the contents of each column and
-                // puts the range into the right slot in the column array, by putting it into the slot of that enum number
-                //
                 for (int columnNumber = 1; columnNumber <= lastColumn; columnNumber++)
                 {
-                    if(!sourceWorksheet.Cell(1, columnNumber).IsEmpty())
+                    if (!sourceWorksheet.Cell(1, columnNumber).IsEmpty())
                     {
                         string columnName = (string)sourceWorksheet.Cell(1, columnNumber).Value;
                         switch (columnName.ToLower())
                         {
                             case "sku":
                                 //first makes sure there is no other column with that header name that was already put into a slot
-                                if(columns[(int)columnHeader.sku] == null)
+                                if (columns[(int)columnHeader.sku] == null)
                                 {
                                     //puts it in
                                     columns[(int)columnHeader.sku] = createNewColumnObject(columnName, columnNumber);
@@ -50,12 +95,12 @@ namespace shopifyNonSeasonalFormatter
 
                             case "item name" or "name":
                                 if (columns[(int)columnHeader.itemName] == null)
-                                { columns[(int)columnHeader.itemName] = createNewColumnObject(columnName, columnNumber); }                          
-                                else { showAlert("Column Exists", $"there is already a column with name: {columnName}"); }             
+                                { columns[(int)columnHeader.itemName] = createNewColumnObject(columnName, columnNumber); }
+                                else { showAlert("Column Exists", $"there is already a column with name: {columnName}"); }
                                 break;
 
                             case "size":
-                                if (columns[(int)columnHeader.size] == null) 
+                                if (columns[(int)columnHeader.size] == null)
                                 { columns[(int)columnHeader.size] = createNewColumnObject(columnName, columnNumber); }
                                 else { showAlert("Column Exists", $"there is already a column with name: {columnName}"); }
                                 break;
@@ -101,6 +146,9 @@ namespace shopifyNonSeasonalFormatter
                                 { columns[(int)columnHeader.extraTags] = createNewColumnObject(columnName, columnNumber); }
                                 else { showAlert("Column Exists", $"there is already a column with name: {columnName}"); }
                                 break;
+                            default:
+                                showAlert("Column Name Not Recognized", "no option for column: " + columnName);
+                                break;
                         }
                     }
                     else
@@ -108,94 +156,73 @@ namespace shopifyNonSeasonalFormatter
                         showAlert($"column {columnNumber} is empty", "");
                     }
                 }
-
-                foreach (column column in columns)
-                {
-                    if(column!= null)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        Console.WriteLine(column.columnName.ToUpper());
-                        Console.WriteLine();
-                        foreach (IXLCell cell in column.rows.Cells())
-                        {
-                            Console.WriteLine(cell.Value);
-                        }
-                    }
-                }
-            }
-            //  
-            static column createNewColumnObject(string columnName, int columnNUmber)
-            {
-                //
-                // Makes the range with the (row, column, row, column) overload
-                //
-                IXLRange rows = sourceWorksheet.Range(2, columnNUmber, lastRow, columnNUmber);
-                column newColumn = new column(columnName, rows);
-
-                return newColumn;
-            }
-
-            static void showAlert(string bigMessage, string smallMessage)
-            {
-                Console.WriteLine();
-                Console.WriteLine(smallMessage);
-                Console.WriteLine(bigMessage);
-                Console.WriteLine();
-                Console.ReadLine();
             }
         }
-        public class column
+        static XLWorkbook createStoneEdgeSpreadsheet()
         {
-            public string columnName;
-            public IXLRange rows;
-
-            public column(string columnName, IXLRange rows)
-            {
-                this.rows = rows;
-                this.columnName = columnName;
-            }
+            XLWorkbook wb = new XLWorkbook();
+            IXLWorksheet worksheet = wb.Worksheets.Add();
+            worksheet.Cell(1, 1).Value = "SKU";
+            return wb;
         }
-        public enum columnHeader
+        static XLWorkbook createShopifySpreadsheet()
         {
-            sku,
-            itemName,
-            size,
-            barcode,
-            price,
-            supplierName,
-            productType,
-            gender,
-            color_metafield,
-            color_variant,
-            extraTags
+            XLWorkbook wb = new XLWorkbook();
+
+            return wb;
         }
-        //NEXT:: maybe put the part of the switch statement taht checks if the slot was already used in the column array, move it to the colmancreator method
-        //
-        //
-        //
-        //
-        //
-        //  
-        //  user will check a box if there are variants of color or size, if yes, then set the variants bool to yes
-        // ,and prompts if it's color varuants or size variants and then proceed to use what's in
-        // the color column, (if they choos yes, then that column must not be empty) same for size
-        //
-        //
-        // instead of having the system delete extra columns and have all the formulas, just concantonate any values together from any
-        // "taggable" column object  (just have the system know which ones are taggable) duh
-        //
-        //
-        // maybe better to have the program do all the chesboning of sizes and new products insteasd of the the formulas
-        // so if there is a size column it will read each cell in title and do, if same as previous then do size column info etc, and if no size column, 
-        // then fill in with defaylt title.
-        //
-        // actually can do mixed with variants and without!! if there is a size column, then if the value of the size column for that row is null
-        // then that means that it's a non variant row and fill in default title. also add warning: "some rows in the size column are empty,
-        // are you sure this a mixed type, or are you missing values?
-        //
-        //
-        // If price column is empty, set it as zero, matrixifty doesnt allow price of 0
-        //
+
     }
+    public class column
+    {
+        public string columnName;
+        public IXLRange rows;
+
+        public column(string columnName, IXLRange rows)
+        {
+            this.rows = rows;
+            this.columnName = columnName;
+        }
+    }
+    public enum columnHeader
+    {
+        sku,
+        itemName,
+        size,
+        barcode,
+        price,
+        supplierName,
+        productType,
+        gender,
+        color_metafield,
+        color_variant,
+        extraTags
+    }
+    //NEXT:: maybe put the part of the switch statement taht checks if the slot was already used in the column array, move it to the colmancreator method
+    //
+    //NEXT: put in a validator that makes sure the neccesary columns exist (name, sku) and a warning for columns that are missing
+    //
+    //
+    //
+    //  
+    //  user will check a box if there are variants of color or size, if yes, then set the variants bool to yes
+    // ,and prompts if it's color varuants or size variants and then proceed to use what's in
+    // the color column, (if they choos yes, then that column must not be empty) same for size
+    //
+    //
+    // instead of having the system delete extra columns and have all the formulas, just concantonate any values together from any
+    // "taggable" column object  (just have the system know which ones are taggable) duh
+    //
+    //
+    // maybe better to have the program do all the chesboning of sizes and new products insteasd of the the formulas
+    // so if there is a size column it will read each cell in title and do, if same as previous then do size column info etc, and if no size column, 
+    // then fill in with defaylt title.
+    //
+    // actually can do mixed with variants and without!! if there is a size column, then if the value of the size column for that row is null
+    // then that means that it's a non variant row and fill in default title. also add warning: "some rows in the size column are empty,
+    // are you sure this a mixed type, or are you missing values?
+    //
+    //
+    // If price column is empty, set it as zero, matrixifty doesnt allow price of 0
+    //
 }
